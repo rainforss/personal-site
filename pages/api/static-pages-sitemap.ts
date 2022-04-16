674cc2f5e58e314eae8fec102f7d386c89d0ea37
rainforss <shengyan@ualberta.ca>
2022-04-15 19:12:28 -0600

    added sitemap and robot.txt

---
+++ pages/api/static-pages-sitemap.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { SitemapStream, streamToPromise } from "sitemap";
+
+const siteMapRoute = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const smStream = new SitemapStream({
+      hostname: `https://${req.headers.host}`,
+    });
+
+    smStream.write({ url: "/", changefreq: "weekly", priority: 0.8 });
+    smStream.write({ url: "/about", changefreq: "weekly", priority: 0.8 });
+    smStream.write({ url: "/projects", changefreq: "weekly", priority: 0.8 });
+    smStream.write({ url: "/contact", changefreq: "weekly", priority: 0.8 });
+
+    smStream.end();
+
+    const siteMapOutput = (await streamToPromise(smStream)).toString();
+
+    res.writeHead(200, {
+      "Content-Type": "application/xml",
+    });
+
+    res.end(siteMapOutput);
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+export default siteMapRoute;
