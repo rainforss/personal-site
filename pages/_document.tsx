1bcbf7b189faac4cf1cc99608458805aa4158977
rainforss <shengyan@ualberta.ca>
2022-09-23 11:43:45 -0600

    added tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,11 +14,19 @@ const MyDocument = () => {
           href="https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed&display=swap"
           rel="stylesheet"
         />
+        <div id="dChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis"></div>
+        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script>
+        <div
+          className="d365-mkt-config"
+          style={{ display: "none" }}
+          data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis"
+          data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"
+        ></div>
       </Head>
       <body>
         <Main />
         <NextScript />
-        <script
+        {/* <script
           id="d365-tracking"
           dangerouslySetInnerHTML={{
             __html: `<div id="dChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis"></div><script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script><div class="d365-mkt-config" style="display:none" data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis" data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"></div>`,
@@ -29,7 +37,7 @@ const MyDocument = () => {
           dangerouslySetInnerHTML={{
             __html: `<script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script> <div class="d365-mkt-config" style="display:none" data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis" data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"> </div>`,
           }}
-        />
+        /> */}
       </body>
     </Html>
   );
