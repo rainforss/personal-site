b274a03b44440fa5b19c641b1c4b371152020011
rainforss <shengyan@ualberta.ca>
2022-09-23 11:30:31 -0600

    added tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,12 @@ const MyDocument = () => {
             __html: `<div id="dChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis"></div><script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script><div class="d365-mkt-config" style="display:none" data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis" data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"></div>`,
           }}
         />
+        <script
+          id="d365-form-tracking"
+          dangerouslySetInnerHTML={{
+            __html: `<script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script> <div class="d365-mkt-config" style="display:none" data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis" data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"> </div>`,
+          }}
+        />
       </body>
     </Html>
   );
