4b3065ce14f59a8b8de0e9552fd2add44ef4f458
rainforss <shengyan@ualberta.ca>
2022-09-23 11:22:52 -0600

    added tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,12 @@ const MyDocument = () => {
       <body>
         <Main />
         <NextScript />
+        <script
+          id="d365-tracking"
+          dangerouslySetInnerHTML={{
+            __html: `<div id="dChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis"></div><script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script><div class="d365-mkt-config" style="display:none" data-website-id="ChSPZufhxuKY7ZVuvGwVVm5t7r3_UptQ-FbsB_LJvis" data-hostname="53b5d92930b045c68a07d11ed0e50c9c.svc.dynamics.com"></div>`,
+          }}
+        />
       </body>
     </Html>
   );
